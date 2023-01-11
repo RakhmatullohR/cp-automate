2d0499f9c85550137fd7abe2807f9221f671948f
RakhmatilloRR <rrr.engineer.94@gmail.com>
2023-01-11 13:31:09 +0500

    some bugs was fixed

--- routes/index.js
+++ routes/index.js
@@ -48,5 +48,8 @@ router.get('/stop-all', async function (req, res, next) {
   isOn = false;
   return await res.send(isOn);
 });
+router.get('/', async function (req, res, next) {
+  return await res.json('cargoprime automate');
+});
 
 module.exports = router;
